50ac9dbf568a61359b3a429029cf59d959791e7c
harshilkumar patel <harshil912@gmail.com>
2017-06-21 01:57:28 +0530

    rm class

--- hacker_rank/algorithms/arrays/crush.java
+++ hacker_rank/algorithms/arrays/crush.java
@@ -5,28 +5,36 @@ import java.util.*;
 public class crush{
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		long n,m,a,b,c,max;
-
+		int n,m,a,b,max;
 		n=s.nextInt();
 		m=s.nextInt();
 		max = 0;
 
+		int[] c = new int[m];
 		int[] seq = new int[n];
+		int[][] pre_matrix = new int[m][n];
 
 		for (int i=0;i<m ;i++ ) {
 			a=s.nextInt();
 			b=s.nextInt();
-			c=s.nextInt();
+			c[i]=s.nextInt();
 			for (int j=a-1;j<b ;j++ ) {
-				if(j>0 && j<n){
-					seq[j] += c;
-					if (seq[j]>max) {
-						seq[j] = max;
+				if(j>=0 && j<=n){
+					pre_matrix[i][j] = 1;
 					}
 				}
 			}
-		}
+		
 
+		for (int i=0; i<n;i++ ) {
+			int value = 0;
+			for (int j=0; j<m;j++ ) {
+				value += pre_matrix[j][i]*c[j];
+			}
+			if(max < value){
+				max = value;
+			}			
+		}
 		System.out.print(max);
 
 	}
